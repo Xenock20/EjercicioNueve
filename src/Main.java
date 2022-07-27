925a632fb6055a2be5379d04275e43a81b51a7e0
Xenock20 <juliosaboredo@gmail.com>
2022-07-27 03:04:28 -0300

    Ejercicio Realizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Inicializacio del objeto Cliente
+        Cliente cliente = new Cliente();
+        cliente.setEdad(20);
+        cliente.setNombre("Julian");
+        cliente.setTelefono("+43-11-21324-43");
+        cliente.setCredito(32432);
+
+        //Inicializacio del objeto Trabajador
+        Trabajador trabajador = new Trabajador();
+        trabajador.setEdad(40);
+        trabajador.setNombre("Jorge");
+        trabajador.setTelefono("+63-11-61374-54");
+        trabajador.setSalario((float) 833.3);
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+    public String getTelefono(){
+        return this.telefono;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+}
+
+class Cliente extends Persona{
+    private int credito;
+
+    public void setCredito(int credito){
+        this.credito = credito;
+    }
+    public int getCredito(){
+        return this.credito;
+    }
+}
+
+class Trabajador extends Persona{
+    private float salario;
+
+    public void setSalario(float salario){
+        this.salario = salario;
+    }
+    public float getSalario(){
+        return this.salario;
     }
 }
